e734eda19d00281787159142f34585bebe62d461
Tim Gerundt <tim.gerundt@montanhydraulik.com>
2023-08-17 15:47:07 +0200

    Change version to 0.3

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 return array (
 	'id' =>             'osticket:close-as-answered',
-	'version' =>        '0.2',
+	'version' =>        '0.3',
 	'name' =>           'Close as answered',
 	'author' =>         'Tim Gerundt',
 	'description' =>    'Mark ticket as answered on close',
